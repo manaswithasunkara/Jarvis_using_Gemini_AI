6f33f233eb66be3c4fc4733806cf687b6da15b33
manaswithasunkara <105747595+manaswithasunkara@users.noreply.github.com>
2024-05-14 16:36:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 key = "AIzaSyBMKdx43DI05yAPA-dGr02G_xncFhVRVNI"
 
 weather = {
-        "X-RapidAPI-Key": "8bab72177cmsh45a1714c4555a24p181d5djsn608adae1c4de",
+        "X-RapidAPI-Key": "<Your Rapid API Key>",
         "X-RapidAPI-Host": "yahoo-weather5.p.rapidapi.com"
     }
 
 mail = {
         "content-type": "application/json",
-        "X-RapidAPI-Key": "8bab72177cmsh45a1714c4555a24p181d5djsn608adae1c4de",
+        "X-RapidAPI-Key": "<Your Rapid API Key>",
         "X-RapidAPI-Host": "mail-sender-api1.p.rapidapi.com"
     }
 
 news = {
-        "X-RapidAPI-Key": "8bab72177cmsh45a1714c4555a24p181d5djsn608adae1c4de",
+        "X-RapidAPI-Key": "<Your Rapid API Key>",
         "X-RapidAPI-Host": "real-time-news-data.p.rapidapi.com"
     }
